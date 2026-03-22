060d43ac6399bd3f969fc0b8efc0b432c4984448
marc-mm <marc.monlleo@estudiantat.upc.edu>
2026-03-22 20:14:58 +0100

    comitting

--- airport.py
+++ airport.py
@@ -1,6 +1,18 @@
+
+class Airport:
+    def __init__(self):
+        self.code = ""
+        self.latitude = 0.0
+        self.longitude = 0.0
+        self.Schengen = False
+
+
+
 def IsSchengenAirport(code):
     Schengen = ('LO', 'EB', 'LK', 'LC', 'EK', 'EE', 'EF', 'LF', 'ED', 'LG', 'EH', 'LH', 'BI','LI', 'EV', 'EY', 'EL', 'LM', 'EN', 'EP', 'LP', 'LZ', 'LJ', 'LE', 'ES', 'LS')
     if code in Schengen:
         True
-    else:
-        False
+
+def SetSchengen(airport):
+    if IsSchengenAirport(airport.code) == True:
+        airport.Schengen = True
