e3a793f20d79355326085a3e3b76bba8732f6272
maxarauzo-upc <max.arauzo@estudiantat.upc.edu>
2026-03-23 22:57:01 +0100

    airpot.py amb logo EETAC

--- airport.py
+++ airport.py
@@ -8,6 +8,44 @@ class Airport:
 
 airport = Airport()
 
+
+def parse_coordinate(coord_str):
+    # Aquesta funció tradueix coses com 'N635906' o 'W0223620' a decimals
+    direction = coord_str[0]  # Agafa la primera lletra (N, S, E, W)
+
+    # Separem els graus, minuts i segons tallant el text
+    degrees = float(coord_str[1:-4])
+    minutes = float(coord_str[-4:-2])
+    seconds = float(coord_str[-2:])
+
+    # Passem tot a format decimal
+    decimal_val = degrees + (minutes / 60) + (seconds / 3600)
+
+    # Si és Sud o Oest, la coordenada ha de ser negativa
+    if direction == 'S' or direction == 'W':
+        decimal_val = -decimal_val
+
+    return decimal_val
+
+
+def LoadAirports(filename):
+    airports = []
+    file = open(filename, 'r')
+    lines = file.readlines()
+
+    for line in lines[1:]:  # Saltem la línia 0 (l'encapçalament)
+        w = line.split()
+        if len(w) == 3:
+            airport = Airport()
+            airport.code = w[0]
+            # Utilitzem el "traductor" per llegir les coordenades
+            airport.latitude = parse_coordinate(w[1])
+            airport.longitude = parse_coordinate(w[2])
+            airports.append(airport)
+
+    file.close()
+    return airports
+
 def IsSchengenAirport(code):
     Schengen = ('LO', 'EB', 'LK', 'LC', 'EK', 'EE', 'EF', 'LF', 'ED', 'LG', 'EH', 'LH', 'BI', 'LI', 'EV', 'EY', 'EL', 'LM', 'EN', 'EP', 'LP', 'LR', 'LZ', 'LJ', 'LE', 'LS', 'ES')
     # AFEGIM [:2] perquè agafi només les dues primeres lletres del codi
