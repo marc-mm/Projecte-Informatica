84fe03ae3913690f8fc65e88a8a1f0b1f479e136
maxarauzo-upc <max.arauzo@estudiantat.upc.edu>
2026-03-23 22:24:24 +0100

    airpot.py amb logo EETAC

--- airport.py
+++ airport.py
@@ -20,25 +20,42 @@ def PrintAirport(airport):
     print(airport)
 
 
+def parse_coordinate(coord_str):
+    # Aquesta funció tradueix coses com 'N635906' o 'W0223620' a decimals
+    direction = coord_str[0]
+
+
+    degrees = float(coord_str[1:-4])
+    minutes = float(coord_str[-4:-2])
+    seconds = float(coord_str[-2:])
+
+    # Passem tot a format decimal
+    decimal_val = degrees + (minutes / 60) + (seconds / 3600)
+
+    # Si és Sud o Oest, la coordenada ha de ser negativa
+    if direction == 'S' or direction == 'W':
+        decimal_val = -decimal_val
+
+    return decimal_val
+
+
 def LoadAirports(filename):
     airports = []
     file = open(filename, 'r')
     lines = file.readlines()
 
-    for line in lines[1:]:
-
+    for line in lines[1:]:  # Saltem la línia 0 (l'encapçalament)
         w = line.split()
-
         if len(w) == 3:
             airport = Airport()
             airport.code = w[0]
-            airport.latitude = float(w[1])
-            airport.longitude = float(w[2])
+            # Utilitzem el "traductor" per llegir les coordenades
+            airport.latitude = parse_coordinate(w[1])
+            airport.longitude = parse_coordinate(w[2])
             airports.append(airport)
 
     file.close()
     return airports
-
 def SaveSchengenAirports(filename, airports):
     file = open(filename, 'w')
     for item in airports:
